5928e6c7dbe3643e1f15eb8a5f1f35feffc85efe
blocki <122746573+peco21@users.noreply.github.com>
2023-06-22 18:02:07 +0200

    update existing items

--- server/backend/management/commands/import_cwe_entries.py
+++ server/backend/management/commands/import_cwe_entries.py
@@ -7,12 +7,14 @@ from backend.models.cwe import EntryType
 
 
 class Command(BaseCommand):
-    help = 'Import cwe entries'
+    help = "Import cwe entries"
     cwe_version = "4.9"
     namespace = "http://cwe.mitre.org/cwe-6"
 
     def handle(self, *args, **options):
-        tree = ET.parse(os.path.join(settings.BASE_DIR, "resources/cwec_v%s.xml" % self.cwe_version))
+        tree = ET.parse(
+            os.path.join(settings.BASE_DIR, "resources/cwec_v%s.xml" % self.cwe_version)
+        )
         root = tree.getroot()
         for weakness in root.iter("{%s}Weakness" % self.namespace):
             cwe_id = weakness.attrib["ID"]
@@ -26,6 +28,12 @@ class Command(BaseCommand):
                     extended_description = child.text
             if not extended_description:
                 extended_description = ""
-            obj, _ = CWE.objects.update_or_create(cwe_id=cwe_id, defaults={
-                "name": name, "description": description, "extended_description": extended_description,
-                "entry_type": EntryType.WEAKNESS})
+            CWE.objects.update_or_create(
+                cwe_id=cwe_id,
+                defaults={
+                    "name": name,
+                    "description": description,
+                    "extended_description": extended_description,
+                    "entry_type": EntryType.WEAKNESS,
+                },
+            )
--- server/checklists/management/commands/import_checklists.py
+++ server/checklists/management/commands/import_checklists.py
@@ -4,10 +4,6 @@ from django.core.management.base import BaseCommand
 from checklists import models
 
 
-# TODO:
-# update categories, items etc when exists
-
-
 class Command(BaseCommand):
     help = "Import checklists from directory into PeCoReT"
 
@@ -21,10 +17,12 @@ class Command(BaseCommand):
             # read info.yaml category.file
             with open(path, "r", encoding="utf-8") as my_file:
                 category_yaml = yaml.safe_load(my_file)
-            category, _created = models.Category.objects.get_or_create(
-                name=category_yaml["name"],
+            category, _created = models.Category.objects.update_or_create(
                 category_id=category_yaml["id"],
-                summary=category_yaml.get("summary"),
+                defaults={
+                    "name": category_yaml["name"],
+                    "summary": category_yaml.get("summary"),
+                },
             )
             self.import_category_items(
                 category_yaml.get("items", []), category, items_directory
@@ -34,11 +32,10 @@ class Command(BaseCommand):
         for item_id in item_ids:
             item_file = Path(directory) / f"{item_id}.md"
             with open(item_file, "r", encoding="utf-8") as my_file:
-                models.Item.objects.get_or_create(
+                models.Item.objects.update_or_create(
                     category=category,
                     item_id=item_id,
-                    name=item_id,
-                    description=my_file.read(),
+                    defaults={"name": item_id, "description": my_file.read()},
                 )
 
     def import_checklists(self, directory):
@@ -47,11 +44,13 @@ class Command(BaseCommand):
             with open(path, "r", encoding="utf-8") as my_file:
                 yaml_data = yaml.safe_load(my_file)
             for data in yaml_data:
-                checklist, _created = models.Checklist.objects.get_or_create(
-                    name=data["name"], checklist_id=data["id"]
+                checklist, _created = models.Checklist.objects.update_or_create(
+                    checklist_id=data["id"], defaults={"name": data["name"]}
                 )
                 category_names = data["categories"]
-                categories = models.Category.objects.filter(category_id__in=category_names)
+                categories = models.Category.objects.filter(
+                    category_id__in=category_names
+                )
                 for category in categories:
                     checklist.categories.add(category)
                     checklist.save()
